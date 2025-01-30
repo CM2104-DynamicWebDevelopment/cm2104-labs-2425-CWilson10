d64e942c8470b35af900a191c1f6b0f0ac086ca3
Caelen Wilson <c.wilson29@rgu.ac.uk>
2025-01-30 11:05:09 +0000

    lab11

--- lab_11/server.js
+++ lab_11/server.js
@@ -17,7 +17,7 @@ app.get('/joke', function(req, res){
 
 app.get('/add', function(req, res){
         var x = parseInt(req.query.x);
-        var y = parseInt(req.query.x);
+        var y = parseInt(req.query.y);
     res.send("X + Y="+(x+y));
 })
 
