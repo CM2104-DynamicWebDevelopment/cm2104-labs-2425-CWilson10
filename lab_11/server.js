defab8f6c490826c8af69a6e2a6c32aba226c9b3
Caelen Wilson <c.wilson29@rgu.ac.uk>
2025-01-30 10:53:17 +0000

    lab11

--- lab_11/server.js
+++ lab_11/server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var jokes = require('knock-knock-jokes')
 var app = express();
-var randomJoke = knockKnockJokes();
+var randomJoke = jokes();
 
 app.get('/', function(req, res){
     res.send("Hello world! by express");
