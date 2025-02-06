8d12edc7e253c7abd8c205e95055c69af258cbad
Caelen Wilson <c.wilson29@rgu.ac.uk>
2025-02-06 10:48:26 +0000

    lab12

--- lab_12/server.js
+++ lab_12/server.js
@@ -2,12 +2,28 @@ var express = require('express');
 var app = express();
 var SpotifyWebApi = require('spotify-web-api-node')
 
-app.use(express.static('public'))
-
 var spotifyApi = new SpotifyWebApi({
     clientId: '5d48a99a1bd648fb87628ed3473317d9',
     clientSecret: 'bc863329684f49d3860e8f5615426430'
 });
+
+app.use(express.static('public'))
+
+// Retrieve an access token
+spotifyApi.clientCredentialsGrant().then(
+    function (data) {
+        console.log('The access token expires in ' + data.body['expires_in']);
+        console.log('The access token is ' + data.body['access_token']);
+        // Save the access token so that it's used in future calls
+        spotifyApi.setAccessToken(data.body['access_token']);
+    },
+    function (err) {
+        console.log(
+            'Something went wrong when retrieving an access token',
+            err.message
+        );
+    }
+);
 
 async function getTracks(searchterm, res) {
     spotifyApi.searchTracks(searchterm)
