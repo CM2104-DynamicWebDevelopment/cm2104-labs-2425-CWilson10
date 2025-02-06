0dc153901572f859c298a4f55e84d665d1911792
Caelen Wilson <c.wilson29@rgu.ac.uk>
2025-02-06 10:38:47 +0000

    lab12

--- lab_12/server.js
+++ lab_12/server.js
@@ -1,8 +1,29 @@
 var express = require('express');
 var app = express();
- app.use(express.static('public'))
- app.get('/'
-, function(req, res){
- res.send("Hello world! by express");
+var SpotifyWebApi = require('spotify-web-api-node')
+
+var spotifyApi = new SpotifyWebApi({
+    clientId: '5d48a99a1bd648fb87628ed3473317d9',
+    clientSecret: 'bc863329684f49d3860e8f5615426430'
+});
+
+app.use(express.static('public'))
+
+async function getTracks(searchterm, res) {
+    spotifyApi.searchTracks(searchterm)
+        .then(function (data) {
+            res.send(JSON.stringify(data.body));
+        }, function (err) {
+            console.error(err);
+        });
+}
+
+app.get('/'
+    , function (req, res) {
+        res.send("Hello world! by express");
+    });
+
+app.get('/searchLove', function (req, res) {
+    getTracks('love', res);
 });
 app.listen(8080);
