3389fb9fe39a0eb07d953df59782e2e0f776db62
Caelen Wilson <c.wilson29@rgu.ac.uk>
2025-02-13 10:52:43 +0000

    lab13 delete

--- lab_13/server.js
+++ lab_13/server.js
@@ -57,3 +57,10 @@ app.post('/search', function (req, res) {
     });
 });
 
+app.post('/delete', function (req, res) {
+    db.collection('quotes').deleteOne(req.body, function (err, result) {
+        if (err) throw err;
+        res.redirect('/');
+    });
+    console.log(req.body, "deleted")
+});
